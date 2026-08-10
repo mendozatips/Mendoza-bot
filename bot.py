cda190275838eae88fab335c05cb2baccd28ab6c
mendozatips <paizinhotips01@gmail.com>
2026-08-09 21:51:23 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ MENSAGEM_GESTAO = """
 PALPITES = [
 
     {
-        "horario": "20:59",
+        "horario": "21:53",
         "odd": "5.75",
         "gestao": "2%",
         "imagem": "bilhete01.jpg",
